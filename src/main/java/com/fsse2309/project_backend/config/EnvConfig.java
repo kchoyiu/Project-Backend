b0c64c790587c83e6dfc34703712f8e897dca384
kchoyiu <kchoyiu@gmail.com>
2023-12-28 10:16:41 +0800

    Full Backend

---
+++ src/main/java/com/fsse2309/project_backend/config/EnvConfig.java
@@ -0,0 +1,6 @@
+package com.fsse2309.project_backend.config;
+
+public class EnvConfig {
+    public static final String prodEnvBaseUrl = "http://fsse2309-project-joe.s3-website-ap-southeast-1.amazonaws.com";
+    public static final String devEnvBaseUrl = "http://localhost:5173";
+}
